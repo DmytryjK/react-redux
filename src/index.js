621c404da5b3a04c0b67507400e18fc1d563c0ee
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-03-17 22:05:51 +0200

    practice with basic redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-console.log('hello redux');
+const initialState = 0;
+
+const reducer = (state, action) => {
+	if (action.type === 'INCREEMENT') {
+		return state + 1;
+	}
+	return 0;
+}
+
+let state = reducer(initialState, {type: 'INCREEMENT'});
+console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
